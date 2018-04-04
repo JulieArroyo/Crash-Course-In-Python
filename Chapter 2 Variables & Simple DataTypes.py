8618f1b3e53aec5b18b4ba82876d7db97b502a8f
Julianne Arroyo <jnarroyo26@gmail.com>
2018-04-03 20:51:35 -0700

    Starting the notes of Chapter Two

---
+++ Chapter 2 Variables & Simple DataTypes.py
@@ -0,0 +1,11 @@
+
+
+# learning variables and print functions
+# Every variable holds a value which is then associated with that variable
+# Naming variables should be short, descriptive
+# you can use underscores instead of spaces
+# Do not use python keywords
+# a .py file must start with a letter, usually lowercase
+
+puppy = "Honey is Sleeping"
+print(puppy)

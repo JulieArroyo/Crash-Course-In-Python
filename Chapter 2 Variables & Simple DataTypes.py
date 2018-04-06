4cece524a30956ebfcbbc04fec158f68b12af052
Julianne Arroyo <jnarroyo26@gmail.com>
2018-04-05 20:41:35 -0700

    Starting the notes of Chapter Two

--- Chapter 2 Variables & Simple DataTypes.py
+++ Chapter 2 Variables & Simple DataTypes.py
@@ -1,8 +1,9 @@
 
 
-# learning variables and print functions
+# ############### VARIABLES AND SIMPLE DATA TYPES - Chapter 2 ************** *
 # Every variable holds a value which is then associated with that variable
-# Naming variables should be short, descriptive
+
+# NAMING VARIABLES should be short, descriptive
 # you can use underscores instead of spaces
 # Do not use python keywords
 # a .py file must start with a letter, usually lowercase
@@ -10,3 +11,23 @@
 puppy = "Honey is Sleeping"
 print(puppy)
 #this prints Honey is Sleeping
+
+
+# CHANGING THE VARIABLE
+# You can change the value of a variable at anytime. Python keeps track of the current value.
+# Traceback - A record of where the interperter ran into trouble.
+
+brah = "Brie is Cold!"
+print(brah)
+
+brah = "Sloppy way to say 'bro'"
+print(brah)
+
+message = "Hello Python World!!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+message = "Hello Python Crash Course Reader!"
+print(message)
